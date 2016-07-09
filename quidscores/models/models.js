bf802e7272efd2463c4059a1be2d54161f419b7c
Isabella Gong <isabella.gong@gmail.com>
2016-07-09 16:23:10 -0400

    adding more models

--- quidscores/models/models.js
+++ quidscores/models/models.js
@@ -8,9 +8,42 @@ var userSchema = mongoose.Schema({
   firstName: String,
   lastName: String,
   username: { type: String, index: { unique: true } },
-  password: String, //Hashed
+  password: String,
+  facebookId: String
 });
 
+var teamSchema = mongoose.Schema({
+	name: String,
+	region: String,
+	roster: {
+		
+	}
+});
+
+var playerSchema = mongoose.Schema({
+	name: String,
+	region: String,
+	mainPosition: {
+		type: String,
+		enum: ['Keeper', 'Chaser', 'Beater', 'Seeker']
+	},
+	otherPositions: {
+		type: [String]
+	},
+	
+});
+
+var tournamentSchema = mongoose.Schema({
+	name: String,
+	date: Date,
+	location: String,
+	games: {
+		type: [Schema.Types.ObjectId]
+	}
+})
+
 module.exports = {
-  User: mongoose.model('User', userSchema)
+  User: mongoose.model('User', userSchema),
+  Team: mongoose.model('Team', teamSchema),
+  Player: mongoose.mode('Player', playerSchema)
 };
