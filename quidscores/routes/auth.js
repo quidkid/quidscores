e06d164998386f6c8c38070156f874a40244f7f3
Julian Mullins <mullinsjulian@gmail.com>
2016-07-09 15:18:13 -0400

    bootstrap+hbs

--- quidscores/routes/auth.js
+++ quidscores/routes/auth.js
@@ -2,7 +2,6 @@
 var express = require('express');
 var router = express.Router();
 var User = require('../models/user');
-var Shipping = require('../models/shipping');
 
 
 module.exports = function(passport) {
